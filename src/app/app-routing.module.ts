f89f45d11d01fd32a2fa05b1389f7b1d918920d8
Vikrant <vikrantyt6380@gmail.com>
2023-06-07 23:14:35 +0530

    basic folder structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { PartyPageComponent } from './party-page/party-page.component';
 
 const routes: Routes = [
   {path:'home',component:HomePageComponent},
   {path:'',redirectTo:'home',pathMatch:'full'},
- 
-  // {path:'home',component:HomePageComponent}
+   {path:'party/:name',component:PartyPageComponent,pathMatch:'full'}
 ];
 
 @NgModule({
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit{
+  constructor(private router: Router) { }
 
+partyDetails:any
+
+ngOnInit(): void {
+  this.partyDetails=[
+    {
+      name:"BANK A",
+      location:"India",
+    },
+    {
+      name:"BANK B",
+      location:"US",
+    },
+    {
+      name:"BANK C",
+      location:"Germany",
+    },
+    {
+      name:"BANK D",
+      location:"India",
+    }
+  ]
+  
+}
+
+login(value:any){
+  this.router.navigate(['/party/:'+value]);
+}
 }
